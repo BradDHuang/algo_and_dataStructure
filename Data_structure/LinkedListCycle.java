5eea6db24d77f9400f99369e0694d6a435ce6d06
BradHuang <adghbc01@tamu.edu>
2018-04-21 18:16:40 +0000

    slow/fast pointers; greedy

--- Data_structure/LinkedListCycle.java
+++ Data_structure/LinkedListCycle.java
@@ -22,28 +22,36 @@ public class Main {
 
     public static boolean hasCycle(ListNode head) {
         // write your code here
-        if (head == null || head.next == null) {
-            return false;
-        }
-
-        ListNode fast, slow;
-
-        slow = head;
-        fast = head.next;
-        while (fast != slow) {
-            if (fast == null || fast.next == null)
-                return false;
+        
+        // if (head == null || head.next == null) {
+        //     return false;
+        // }
+        // ListNode fast, slow;
+        // slow = head;
+        // fast = head.next;
+        // while (fast != slow) {
+        //     if (fast == null || fast.next == null)
+        //         return false;
+        //     fast = fast.next.next;
+        //     slow = slow.next;
+        // }
+        // return true;
+        
+        if (head == null) return false;
+        ListNode fast = head;
+        ListNode slow = head;
+        while (slow.next != null && fast.next != null && fast.next.next != null) {
             fast = fast.next.next;
             slow = slow.next;
+            if (fast == slow) return true;
         }
-        return true;
+        return false;
     }
 
     public static void main(String[] args) {
         // write your code here
 
         ListNode a = new ListNode(1);
-
         a.next = new ListNode(2);
 //        ListNode b = a.next;
 //        b.next = new ListNode(3);
@@ -51,8 +59,8 @@ public class Main {
         c.next = new ListNode(3);
         
         c.next.next = a;
-        
         System.out.println(hasCycle(a));
 
     }
+    
 }
