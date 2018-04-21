a5e4078f0b47d27850620ca54160cb9eb4ea2c3c
BradHuang <adghbc01@tamu.edu>
2018-04-21 19:27:42 +0000

    BT post-order

---
+++ Lintcode/Tree/BTpostorderTraversal.java
@@ -0,0 +1,54 @@
+package com.company;
+
+// lc 68.
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+
+//    static List<Integer> res;
+//    public static List<Integer> postorderTraversal(TreeNode root) {
+//        res = new ArrayList<>();
+//        if (root == null) return res;
+//        helper(root);
+//        return res;
+//    }
+//    private static void helper(TreeNode root) {
+//        if (root == null) return;
+//        helper(root.left);
+//        helper(root.right);
+//        res.add(root.val);
+//    }
+
+    // without recursion. use Stack!
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+//        LinkedList<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+
+            res.add(0, cur.val);
+//            res.addFirst(cur.val);
+
+            if (cur.left != null) stack.push(cur.left);
+            if (cur.right != null) stack.push(cur.right);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(1);
+        root.right = new TreeNode(2);
+        root.right.left = new TreeNode(3);
+        System.out.println(postorderTraversal(root));
+        
+    }
+
+}
