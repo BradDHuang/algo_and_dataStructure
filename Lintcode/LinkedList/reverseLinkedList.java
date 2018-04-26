1dfa976f2eede2d286c59051710987add925b37b
BradHuang <adghbc01@tamu.edu>
2018-04-26 04:39:58 +0000

    LinkedList reverse in-place.

---
+++ Lintcode/LinkedList/reverseLinkedList.java
@@ -0,0 +1,33 @@
+package com.company;
+
+// leetcode 35.
+
+public class Main {
+
+    public static ListNode reverse(ListNode head) {
+        // write your code here
+        if (head == null) return null;
+        ListNode pre = null;
+        while (head != null) {
+            // ListNode cur = head;
+            ListNode cur = head.next;
+            head.next = pre;
+            pre = head;
+            // head = cur.next;
+            head = cur; // form a cycle!
+        }
+        return pre;
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        ListNode newHead = reverse(head);
+        System.out.println(newHead.val);
+        System.out.println(newHead.next.val);
+        System.out.println(newHead.next.next);
+
+    }
+
+}
