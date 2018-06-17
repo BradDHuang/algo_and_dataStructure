7ec75f7b9b05bddb462365c4bae57ef89a0f172d
BradHuang <adghbc01@tamu.edu>
2018-06-17 02:24:06 +0000

    lc 413 1st working solution.

---
+++ Lintcode/reverseInteger.java
@@ -0,0 +1,35 @@
+
+// lintcode 413. reverse integer
+
+public class Solution {
+    /**
+     * @param n: the integer to be reversed
+     * @return: the reversed integer
+     */
+    public int reverseInteger(int n) {
+        // write your code here
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+        char sign = '+';
+        if (n < 0) {
+            sign = '-';
+        } 
+        int abs = Math.abs(n);
+        long res = 0;
+        // while (abs > 10) {
+        while (abs > 0) {
+            // res += abs/10
+            res = res * 10 + abs % 10;
+            if (res > max) {
+                return 0;
+            }
+            abs /= 10;
+        }
+        if (sign == '-') res *= -1;
+        if (res < min) return 0;
+        return (int) res;
+    }
+}
+
+
+
