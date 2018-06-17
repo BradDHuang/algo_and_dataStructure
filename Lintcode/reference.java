d903037130f6ebcbb6b2c6bb670782f7e8c9d788
BradHuang <adghbc01@tamu.edu>
2018-06-17 20:12:17 +0000

    lc 456 1st working solution.

---
+++ Lintcode/reference.java
@@ -0,0 +1,29 @@
+
+// lc 456. reference
+
+/**
+ * Definition of Node:
+ * class Node {
+ *     public int val;
+ *     public Node(int val) {
+ *         this.val = val;
+ *     }
+ * }
+ */
+public class ReferenceManager {
+    public Node node;
+
+    public void copyValue(Node obj) {
+        // copy value from obj to node
+        
+        // node.value = obj.value;
+        node = new Node(obj.val);
+    }
+
+    public void copyReference(Node obj) {
+        // copy reference from obj to node
+        node = obj;
+    }
+}
+
+
